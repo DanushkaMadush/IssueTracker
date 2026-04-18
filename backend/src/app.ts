2eea7ee3a75d877fd4fc50558a96505ed8868a09
danushka <danmadushzxc@gmail.com>
2026-04-18 23:41:11 +0530

    feat:added issues apis

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,7 @@
 import  express from "express";
 import cors from "cors";
 import authRoutes from "./routes/auth.routes";
+import issueRoutes from "./routes/issue.routes";
 import { errorMiddleware } from "./middleware/error.middleware";
 
 const app = express();
@@ -8,6 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/issues", issueRoutes);
 app.use(errorMiddleware);
 
 app.get("/", (req, res) => {
