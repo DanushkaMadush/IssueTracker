113e497f39f59186c64703dd321c833b33b9eb12
danushka <danmadushzxc@gmail.com>
2026-04-21 12:25:20 +0530

    feat:added statsection to the homepage

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,9 +1,24 @@
-const HomePage = () => {
-    return (
-        <div>
-            <h1>Home Page</h1>
-        </div>
-    );
+// pages/HomePage.tsx
+import React from "react";
+import { colors } from "../theme/colors";
+import StatsSection from "../components/StatsSection";
+
+const HomePage: React.FC = () => {
+  return (
+    <div
+      style={{
+        backgroundColor: colors.background,
+        minHeight: "100vh",
+        padding: "20px",
+      }}
+    >
+      <h1 style={{ color: colors.text, marginBottom: "20px" }}>
+        Issue Tracker
+      </h1>
+
+      <StatsSection />
+    </div>
+  );
 };
 
 export default HomePage;
